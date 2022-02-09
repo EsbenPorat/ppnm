5fd85c44e944ab661255c500903efc480e0b2cc2
Esben Juel Porat <esbenporat@hotmail.com>
2022-02-09 09:31:40 +0100

    hello and math exercises

---
+++ exercises/math/mathE.cs
@@ -0,0 +1,10 @@
+using static System.Math;
+public static class math{
+	static int Main(){
+		double sqrt2 = Sqrt(2.0);
+		double expPi = Exp(PI);
+		double piPowerE = Pow(PI,E);
+		System.Console.Write($"The square root of 2 is {sqrt2} \ne to the power pi is {expPi} \npi to the power e is {piPowerE} \n");
+		return 0;
+	}
+}
