bbe2e7d21fbc400d82e95c4588208dcfdb91fcdc
EsbenPorat <esbenporat@hotmail.com>
2022-05-22 17:20:29 +0200

    Test png for pyxplot

--- exercises/integration/tabulate.cs
+++
@@ -1,6 +0,0 @@
-public static class tabulate{
-	public static void make_table(System.Func<double,double> f, double a, double b, double dx){
-		for(double x=a;x<=b;x+=dx)
-		System.Console.Write($"{x} {f(x)}\n");
-	}
-}
